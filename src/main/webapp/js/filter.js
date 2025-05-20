7c93f29b66767298d538737e37d53c1e5ec8e796
mssuperlol <mssuperlol@gmail.com>
2025-05-20 11:40:58 +0200

    Fixed condition for login and added call to showHomepage

--- src/main/webapp/js/filter.js
+++ src/main/webapp/js/filter.js
@@ -1,5 +1,8 @@
 window.addEventListener("load", () => {
-    if(sessionStorage.getItem("user") == null) {
+    if (sessionStorage.getItem("user_id") === null) {
         window.location.href = "login.html";
+    } else {
+        let showPage = new ShowPage
+        showPage.showHomepage();
     }
 }, false);
