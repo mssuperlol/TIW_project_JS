b2147bf40a80bf5b4415e89696526853788aefba
mssuperlol <mssuperlol@gmail.com>
2025-05-23 17:48:59 +0200

    Added GetEncoding

---
+++ src/main/java/it/polimi/tiw_project_js/utils/GetEncoding.java
@@ -0,0 +1,39 @@
+package it.polimi.tiw_project_js.utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Base64;
+
+import jakarta.servlet.ServletContext;
+
+import org.apache.commons.io.FileUtils;
+
+public class GetEncoding {
+
+    /**
+     * Static method that takes the file from local storage and encodes it into a base64 string
+     *
+     * @param relativeFilePath
+     * @param context
+     * @return
+     * @throws IOException
+     */
+    public static String getFileEncoding(String relativeFilePath, ServletContext context) throws IOException {
+        String folderPath = context.getInitParameter("musicPath");
+        File file = new File(folderPath + relativeFilePath);
+
+        if (!file.exists() || file.isDirectory()) {
+            return null;
+        }
+
+        //Take the byte array of the file
+        byte[] fileContent = FileUtils.readFileToByteArray(file);
+        //Take the base64 string
+        String encodedString = Base64.getEncoder().encodeToString(fileContent);
+
+        //Take the right type of each file
+        encodedString = "data:" + context.getMimeType(relativeFilePath) + ";base64," + encodedString;
+
+        return encodedString;
+    }
+}
