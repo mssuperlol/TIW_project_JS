7bf3a59b8ec318e9b9be9eae3f3cfaa911fbe9c1
mssuperlol <mssuperlol@gmail.com>
2025-05-25 10:00:05 +0200

    Fixed doc

--- src/main/webapp/js/utils.js
+++ src/main/webapp/js/utils.js
@@ -2,9 +2,9 @@
  * Function that handles calls to servlets
  * @param method whether to use GET or POST method of the servlet
  * @param url of the servlet to call
- * @param formElement
- * @param cback
- * @param reset
+ * @param formElement form to send with the url
+ * @param cback function to execute once the servlet sends a response
+ * @param reset whether to reset the form after the call
  */
 function makeCall(method, url, formElement, cback, reset = true) {
     let req = new XMLHttpRequest(); // visible by closure
